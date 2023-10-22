9be31cebf4965603a6a1004c6b8a45ec8d5bfd91
Wibi Hanif Wibowo <wibihanifwibowo@gmail.com>
2023-10-22 19:38:49 +0700

    fix rout

--- src/components/router/AccordionRoutes.tsx
+++ src/components/router/AccordionRoutes.tsx
@@ -1,49 +1,75 @@
 import { Accordion, Box, Flex, Text, ThemeIcon, useMantineTheme } from '@mantine/core';
-import { Link } from 'react-router-dom';
+import { Link, Location } from 'react-router-dom';
 import { routes } from '~/routes';
 
-export const AccordionRoutes = () => {
+interface AccordionRoutesProps {
+  location: Location;
+}
+
+export const AccordionRoutes: React.FC<AccordionRoutesProps> = ({ location }) => {
   const theme = useMantineTheme();
+  const parsedNowLocation = location.pathname.split('/')[1];
+
+  let accordionActiveValue;
+
+  routes.forEach(route => {
+    route.subTitleItems.forEach(subTitleItem => {
+      subTitleItem.routeItems.forEach(routeItem => {
+        const matchRoute = routeItem.path === parsedNowLocation;
+
+        if (matchRoute) {
+          accordionActiveValue = subTitleItem.subTitle;
+        }
+      });
+    });
+  });
 
   const renderAccordion = () => {
     return routes.map((route, index) => {
       return (
-        <>
-          <Box key={index}>
-            <Text>{route.title}</Text>
-          </Box>
-          {route.subTitleItems.map(subTitle => {
+        <Box key={index}>
+          <Text>{route.title}</Text>
+          {route.subTitleItems.map((subTitle, index) => {
             return (
-              <>
-                <Accordion.Item value={subTitle.subTitle} key={subTitle.key}>
+              <Box key={index}>
+                <Accordion.Item value={subTitle.subTitle}>
                   <Accordion.Control>
                     <Flex gap={10}>
                       <ThemeIcon variant="light">{subTitle.icon}</ThemeIcon>
                       <Text fw="bold">{subTitle.subTitle}</Text>
                     </Flex>
                   </Accordion.Control>
-                  {subTitle.routeItems.map(routeItem => {
+                  {subTitle.routeItems.map((routeItem, index) => {
+                    const isActive = parsedNowLocation === routeItem.path;
+
                     return (
-                      <Link to={routeItem.path} style={{ textDecoration: 'none', color: 'black' }}>
+                      <Link
+                        to={routeItem.path}
+                        style={{ textDecoration: 'none', color: 'black' }}
+                        key={index}>
                         <Accordion.Panel
                           sx={{
                             paddingLeft: 30,
                             ':hover': { cursor: 'pointer', backgroundColor: theme.colors.blue[2] },
-                          }}
-                          key={routeItem.key}>
+                            backgroundColor: isActive ? theme.colors.blue[2] : 'none',
+                          }}>
                           <Text>{routeItem.title}</Text>
                         </Accordion.Panel>
                       </Link>
                     );
                   })}
                 </Accordion.Item>
-              </>
+              </Box>
             );
           })}
-        </>
+        </Box>
       );
     });
   };
 
-  return <Accordion variant="filled">{renderAccordion()}</Accordion>;
+  return (
+    <Accordion variant="filled" defaultValue={accordionActiveValue}>
+      {renderAccordion()}
+    </Accordion>
+  );
 };
--- src/components/router/MainRouter.tsx
+++ src/components/router/MainRouter.tsx
@@ -27,16 +27,6 @@ export const MainRouter = () => {
     });
   };
 
-  // const renderMainRoutes = (callback: (subRoute: SubRoute) => React.ReactElement | undefined) => {
-  //   return routes.map(mainRoute => {
-  //     return (
-  //       <Route path={`${mainRoute.path}/*`} key={mainRoute.key}>
-  //         {mainRoute.subRoutes.map(callback)}
-  //       </Route>
-  //     );
-  //   });
-  // };
-
   const renderRoutes = () => {
     return renderMainRoutes(subRoute => {
       return (
@@ -50,76 +40,8 @@ export const MainRouter = () => {
     });
   };
 
-  // const renderPageTitle = () => {
-  //   return renderMainRoutes(subRoute => {
-  //     return (
-  //       <Route
-  //         path={subRoute.path}
-  //         element={<Title>{subRoute.title}</Title>}
-  //         index={subRoute.index}
-  //         key={subRoute.key}
-  //       />
-  //     );
-  //   });
-  // };
-
-  // const renderSidebar = () => {
-  //   if (location.pathname === '/') {
-  //     return routes.map(mainRoute => {
-  //       return (
-  //         <Route path={`${mainRoute.path}/*`} key={mainRoute.key}>
-  //           <Route
-  //             path="*"
-  //             element={routes.map(mainRoute => {
-  //               return (
-  //                 <SidebarLink
-  //                   key={mainRoute.key}
-  //                   label={mainRoute.title}
-  //                   to={mainRoute.path}
-  //                   icon={mainRoute.icon}
-  //                   active={mainRoute.path === '/'}
-  //                 />
-  //               );
-  //             })}
-  //           />
-  //         </Route>
-  //       );
-  //     });
-  //   }
-
-  //   return routes.map(mainRoute => {
-  //     return (
-  //       <Route path={`${mainRoute.path}/*`} key={mainRoute.key}>
-  //         <Route
-  //           path="*"
-  //           element={mainRoute.subRoutes.map(subRoute => {
-  //             if (!subRoute.isSidebarMenu) return null;
-
-  //             const isActive =
-  //               `/${mainRoute.path}${subRoute.path ? '/' + subRoute.path : ''}` ===
-  //               location.pathname;
-
-  //             return (
-  //               <SidebarLink
-  //                 key={subRoute.key}
-  //                 label={subRoute.title}
-  //                 to={subRoute.path}
-  //                 icon={subRoute.icon}
-  //                 active={isActive}
-  //               />
-  //             );
-  //           })}
-  //         />
-  //       </Route>
-  //     );
-  //   });
-  // };
-
   return (
-    <GSIShell sidebarMenus={<AccordionRoutes />} location={location}>
-      {/* <Box pb="lg" mt={5}>
-        <Routes>{renderPageTitle()}</Routes>
-      </Box> */}
+    <GSIShell sidebarMenus={<AccordionRoutes location={location} />} location={location}>
       <Routes>{renderRoutes()}</Routes>
     </GSIShell>
   );
