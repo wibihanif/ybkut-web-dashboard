82aea1c4800f640200cda5108f6694cc6a8ba3e6
Veanus Nathan <veanusnathan.work@gmail.com>
2023-10-26 17:07:42 +0700

    resolve

--- src/features/asset/component/table/tableSection.tsx
+++ src/features/asset/component/table/tableSection.tsx
@@ -1,11 +1,8 @@
-import { Flex, Grid, Pagination, Paper, Text } from '@mantine/core';
+import { Grid, Paper, Text } from '@mantine/core';
 import { TotalScrapTable } from './TotalScrapTable';
 import { ItemDepreciationTable } from './ItemDepresiationTable';
-import { useState } from 'react';
 
 export const TableSection = () => {
-  const [page, setPage] = useState<number>(0);
-
   return (
     <>
       <Grid mt={20}>
@@ -21,12 +18,9 @@ export const TableSection = () => {
         <Grid.Col span={7}>
           <Paper
             style={{ borderRadius: 2, boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.1)', padding: 20 }}>
-            <Flex justify="space-between">
-              <Text color="#61677A" fw="bold" fz="sm" pb={20}>
-                List Item Depreciation
-              </Text>
-              <Pagination value={page} onChange={setPage} total={2} sx={{ alignSelf: 'center' }} />
-            </Flex>
+            <Text color="#61677A" fw="bold" fz="sm" pb={20}>
+              List Item Depreciation
+            </Text>
             <ItemDepreciationTable />
           </Paper>
         </Grid.Col>
