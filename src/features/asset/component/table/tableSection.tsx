be5de7e376c218347449d061295636e9dc249af0
Veanus Nathan <veanusnathan.work@gmail.com>
2023-10-26 17:03:24 +0700

    feat(pagination): add pagination

--- src/features/asset/component/table/tableSection.tsx
+++ src/features/asset/component/table/tableSection.tsx
@@ -1,8 +1,11 @@
-import { Grid, Paper, Text } from '@mantine/core';
+import { Flex, Grid, Pagination, Paper, Text } from '@mantine/core';
 import { TotalScrapTable } from './TotalScrapTable';
 import { ItemDepreciationTable } from './ItemDepresiationTable';
+import { useState } from 'react';
 
 export const TableSection = () => {
+  const [page, setPage] = useState<number>(0);
+
   return (
     <>
       <Grid mt={20}>
@@ -18,9 +21,12 @@ export const TableSection = () => {
         <Grid.Col span={7}>
           <Paper
             style={{ borderRadius: 2, boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.1)', padding: 20 }}>
-            <Text color="#61677A" fw="bold" fz="sm" pb={20}>
-              List Item Depreciation
-            </Text>
+            <Flex justify="space-between">
+              <Text color="#61677A" fw="bold" fz="sm" pb={20}>
+                List Item Depreciation
+              </Text>
+              <Pagination value={page} onChange={setPage} total={2} sx={{ alignSelf: 'center' }} />
+            </Flex>
             <ItemDepreciationTable />
           </Paper>
         </Grid.Col>
