9461dfbb3236eae71667b312e16150c1c931bebe
Arifitanto Oktarian Hafayer <arifitantotito@gmail.com>
2024-06-12 03:03:12 +0700

    color point and additional summary

--- src/components/map/Map.tsx
+++ src/components/map/Map.tsx
@@ -1,14 +1,49 @@
-import { Circle, FeatureGroup, MapContainer, Popup, TileLayer } from 'react-leaflet';
+import { Circle, FeatureGroup, MapContainer, Popup, TileLayer, useMap } from 'react-leaflet';
+import L from 'leaflet';
 import '../../style.css';
 import 'leaflet/dist/leaflet.css';
 import { SchoolLocation } from '~/features/nusantara/types';
+import { useEffect } from 'react';
 
 interface MapsProps {
   schoolLocations: SchoolLocation[];
 }
 
+const Legend: React.FC = () => {
+  const map = useMap();
+
+  useEffect(() => {
+    const legend = new L.Control({ position: 'bottomright' });
+
+    legend.onAdd = function () {
+      const div = L.DomUtil.create('div', 'info legend');
+      const colors = ['green', 'yellow', 'red'];
+      const labels = ['Full Class', 'Partially Available Class', 'Available Class'];
+
+      for (let i = 0; i < colors.length; i++) {
+        div.innerHTML +=
+          '<i style="background:' +
+          colors[i] +
+          '; width: 18px; height: 18px; border-radius: 50%; float: left; margin-right: 8px;"></i> ' +
+          labels[i] +
+          '<br>';
+      }
+
+      return div;
+    };
+
+    legend.addTo(map);
+
+    return () => {
+      legend.remove();
+    };
+  }, [map]);
+
+  return null;
+};
+
 export const Maps: React.FC<MapsProps> = ({ schoolLocations }) => {
-  const center: any = ['-0.9070', '117.8231'];
+  const center: [number, number] = [-0.907, 117.8231];
 
   return (
     <MapContainer
@@ -21,23 +56,28 @@ export const Maps: React.FC<MapsProps> = ({ schoolLocations }) => {
       zoomControl={false}
       touchZoom={false}
       keyboard={false}
+      attributionControl={false}
       tap={false}>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-      {schoolLocations?.map(schoolLocation => {
+      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+      {schoolLocations?.map((schoolLocation, index) => {
+        const circleColor =
+          schoolLocation.full === '0' ? 'green' : schoolLocation.full === '1' ? 'yellow' : 'red';
+
         return (
-          <FeatureGroup>
+          <FeatureGroup key={index}>
             <Popup>{schoolLocation.area}</Popup>
             <Circle
-              center={[schoolLocation?.lat as any, schoolLocation.long as any]}
+              center={[schoolLocation.lat as any, schoolLocation.long as any]}
               radius={200}
-              pathOptions={{ color: 'purple', weight: 15 }}
+              pathOptions={{
+                color: circleColor,
+                weight: 15,
+              }}
             />
           </FeatureGroup>
         );
       })}
+      <Legend />
     </MapContainer>
   );
 };
