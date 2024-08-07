2f73c87ab372df9c079034f90994fde4fb9b3897
Arifitanto Oktarian Hafayer <arifitantotito@gmail.com>
2024-08-07 22:09:42 +0700

    prettier summary kipl

--- src/features/kipl/component/SummarySection.tsx
+++ src/features/kipl/component/SummarySection.tsx
@@ -194,6 +194,7 @@ export const SummarySection: React.FC<SummarySectionProps> = ({
             key={summaryItem.title}
             bg="white"
             style={{
+              position: 'relative',
               borderRadius: 8,
               boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.1)',
               transition: 'transform 0.3s ease-in-out',
@@ -203,44 +204,47 @@ export const SummarySection: React.FC<SummarySectionProps> = ({
                 cursor: 'pointer',
                 transform: 'scale(1.1)',
               },
+              '::before': {
+                content: '""',
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                right: 0,
+                height: '15px', // Adjust the height as needed
+                backgroundColor: '#ceb28d',
+                borderTopLeftRadius: 8,
+                borderTopRightRadius: 8,
+              },
             }}
             onClick={() => navigateToCertainScreen(summaryItem.route)}>
-            <Flex gap={20}>
-              <Box bg="transparent" px={12}>
-                <ThemeIcon variant="light" size="xl" radius="xl" color="#ceb28d" my={15}>
-                  {summaryItem.icon}
-                </ThemeIcon>
-              </Box>
-
-              <div className="w-[180px]">
+            <Flex gap={20} px={12} py={15} className="justify-between items-center">
+              <div className="w-100%">
                 <Box>
-                  <Text fz="md" fw="bold">
+                  <Text fz="lg" fw="bolder">
                     {summaryItem.title}
                   </Text>
-                  <Text fz="lg" color="#7D7C7C" fw="bold">
+                  <Text fz="lg" color="#7D7C7C" fw="bolder">
                     Total: {summaryItem.total(groupedKiplValues[index])}
                   </Text>
-                  <Text fz="lg" color="#7D7C7C" fw="bold">
+                  <Text fz="sm" color="#7D7C7C" fw="bold">
                     Amount: {summaryItem.amount(groupedKiplAmounts[index])}
+                  </Text>
+                  <Text fz="sm" color="#7D7C7C" fw="bold">
+                    Amount: {summaryItem.percentage(groupedKiplPercentages[index])}%
                   </Text>
                 </Box>
               </div>
-              <Center>
-                <div
-                  style={{
-                    display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                    width: '80px',
-                    height: '80px',
-                    borderRadius: '50%',
-                    backgroundColor: '#ceb28d',
-                  }}>
-                  <Text fz="lg" color="#000000" fw="bolder">
-                    <div className="">{summaryItem.percentage(groupedKiplPercentages[index])}%</div>
-                  </Text>
-                </div>
-              </Center>
+
+              <Box bg="transparent">
+                <ThemeIcon
+                  variant="light"
+                  size="50px"
+                  radius="xl"
+                  color="#ceb28d"
+                  style={{ width: '100%', height: '100%' }}>
+                  <div>{summaryItem.icon}</div>
+                </ThemeIcon>
+              </Box>
             </Flex>
           </Box>
         );
